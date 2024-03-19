fc1af495e2e636087a489985ed2ee4bc9039e7a2
SokuRitszZ <1062708758@qq.com>
2024-03-19 22:59:38 +0800

    add alias & install

--- alias.sh
+++ alias.sh
@@ -11,3 +11,15 @@ alias ds="docker start"
 alias di="docker images"
 alias dp="docker ps"
 alias dpa="docker ps -a"
+
+# git
+git_commit() {
+	git commit -m $1
+}
+
+alias gs="git status"
+alias ga="git add ."
+alias gc="git_commit"
+alias gph="git push"
+alias gphf="git push -f"
+alias gpl="git pull"
