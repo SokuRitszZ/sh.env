25e1b2370cff06a3e69d482a98e1ffcd16740857
SokuRitszZ <1062708758@qq.com>
2024-04-11 09:58:21 +0800

    chore: add homebrew install

--- alias.sh
+++ alias.sh
@@ -1,7 +1,6 @@
 # npm
 alias nd="pnpm dev"
-alias nb="pnpm build"
-alias ni="pnpm i"
+alias nb="pnpm build" alias ni="pnpm i"
 alias nid="pnpm i -D"
 alias nr="pnpm rm"
 alias nig="npm i -g"
@@ -11,6 +10,11 @@ alias ds="docker start"
 alias di="docker images"
 alias dp="docker ps"
 alias dpa="docker ps -a"
+docker_kill_rm() {
+	docker kill "$@"
+	docker rm "$@"
+}
+alias dkr="docker_kill_rm"
 
 # git
 git_commit() {
@@ -19,7 +23,17 @@ git_commit() {
 
 alias gs="git status"
 alias ga="git add ."
-alias gc="git_commit"
+alias gc="git_commit -m"
+alias gcl="git clone"
 alias gph="git push"
 alias gphf="git push -f"
 alias gpl="git pull"
+
+# webstorm
+ws_open_folder() {
+	open -na "WebStorm.app" --args "$@"
+}
+alias ws="ws_open_folder"
+
+# os
+alias ss="source ~/.zshrc"
